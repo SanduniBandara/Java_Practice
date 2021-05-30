55e44996c3d23049c4dec31b371baadeb13ffcef
SanduniBandara <85072284+SanduniBandara@users.noreply.github.com>
2021-05-30 22:03:26 +0530

    Add files via upload

---
+++ SumofNumber.java
@@ -0,0 +1,10 @@
+class SumofNumber{
+	public static void main(String [] args){
+		// x = No. of students participated in the lecture 1
+		// y = No. of students partiicpated in the lecture 2
+		//Sum represents the total no. of students partcipated for 2 lectures.
+		int x = 10, y = 11, sum; 
+		sum = x + y;
+		System.out.println(sum);
+	}
+}
