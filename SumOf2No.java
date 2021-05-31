ee83bf8375fe5ad654922a2a00018e6da961d5f2
SanduniBandara <85072284+SanduniBandara@users.noreply.github.com>
2021-05-31 08:43:46 +0530

    Add files via upload

---
+++ SumOf2No.java
@@ -0,0 +1,8 @@
+class SumOf2No {
+	public static void main (String [] args){
+		int x = 5, y = 8, sum; //Defining and assigning values to variables.
+		sum = x + y; //Creating the function to be carried out.
+		System.out.println("Sum of the given numbers 5 and 8 = " + sum);
+		//Displaying the output.
+	}
+}
