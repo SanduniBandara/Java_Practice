771460530a7ea3f35f5e18aca5aa6b5eca8cc4a7
SanduniBandara <85072284+SanduniBandara@users.noreply.github.com>
2021-06-02 21:34:26 +0530

    Add files via upload

---
+++ MultiplicationOf_11_4.java
@@ -0,0 +1,10 @@
+class MultiplicationOf_11_4 {
+	public static void main (String [] args){
+		int x = 11, y = 4 , mul;
+		//Initializing and defining variables.
+		mul = x*y;
+		//Creating the function.
+		System.out.println("Multiplication of 11 by 4 = " + mul);
+		
+	}
+}
