b8d72a13a2a1137ce7ecc678f87580c117ea3e14
SanduniBandara <85072284+SanduniBandara@users.noreply.github.com>
2021-06-06 11:11:55 +0530

    Add files via upload

---
+++ MulNo.java
@@ -0,0 +1,11 @@
+class MulNo {
+	public static void main (String [] args){
+		float a=4, b=5, n;
+		n = (a*b)*(1/2);
+		System.out.println(n);
+		/* Ask from Madam what is the error here. 
+		No errors when compiling. But a run time 
+		error is there. This indicates zero when 
+		multiplied by 1/2. */
+	}
+}
