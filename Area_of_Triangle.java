eb5e1ba3a06a98004857bccd2d5299124b56d477
SanduniBandara <85072284+SanduniBandara@users.noreply.github.com>
2021-06-06 11:00:52 +0530

    Add files via upload

---
+++ Area_of_Triangle.java
@@ -0,0 +1,11 @@
+class Area_of_Triangle {
+  public static void main (String [] args){
+    int b = 12,h = 6, area;
+    area = (b*h)/2;
+    System.out.println("Area of the Triangle given = " + area);
+    /* Here area is set to an integer variable whereas the base and height
+    are defined as integers and given a value.
+    Also the output is displayed in this program */
+    
+  }
+}
---
+++ Dollars_to_Rs.java
@@ -0,0 +1,8 @@
+class Dollars_to_Rs{
+	public static void main (String [] args){
+		int dollar=150, rs=200, val;
+		val = dollar*rs;
+		System.out.println("150 Dollars in Rupees = "+ val);
+		
+	}
+}
