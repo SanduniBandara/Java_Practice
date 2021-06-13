527304d5cf4c0004a719ebdb107e3b5b9db43261
SanduniBandara <85072284+SanduniBandara@users.noreply.github.com>
2021-06-13 18:05:35 +0530

    Add files via upload

---
+++ add_43_79.java
@@ -0,0 +1,12 @@
+class add_43_79 {
+  public static void main(String [] args){
+    //defining the variables and assigning values.
+    int x = 43, y = 79, sum;
+
+    sum = x+y; 
+
+    //Displaying the output.
+    System.out.println("The sum of 43 and 79 = "+sum);
+  }
+
+}
---
+++ mult_7_5.java
@@ -0,0 +1,17 @@
+class mult_7_5 {
+  public static void main (String [] args){
+    //Initializing and defining variables.
+    int x=5, y=7, n;
+    /*Here x is the height of the height of the square,
+    y is the width of the square,
+    and the area of the square is given by n.
+     */
+
+    //inserting the function to be carried out.
+    n= x*y;
+
+    //Displaying the output of the above funtion.
+    System.out.println("Multiplication of 5 by 7 = "+n);
+
+  }
+}
