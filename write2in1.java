030cd17cb2c46f7864841995d22b9f225d1dcea6
SanduniBandara <85072284+SanduniBandara@users.noreply.github.com>
2021-06-13 18:09:33 +0530

    Add files via upload

---
+++ write2in1.java
@@ -0,0 +1,7 @@
+class write2in1 {
+  public static void main (String args []){
+    //Writing two sentences in one line.
+    System.out.println("Jenny is John’s sister.Jenny is a first year undergraduate student in Faculty of Management, University of Ohio");
+    
+  }
+}
---
+++ write2in2.java
@@ -0,0 +1,8 @@
+class write2in2{
+  public static void main (String [] args){
+    //Writing 2 sentences in two lines.
+    System.out.println("Jenny is John’s sister.");
+    System.out.println("John is a third year undergraduate student in Faculty of Science, University of California");
+    
+  }
+}
