dd5404eeca5653f0fecf3c881c3a026034562066
SanduniBandara <85072284+SanduniBandara@users.noreply.github.com>
2021-06-20 21:12:59 +0530

    First Commit

---
+++ PassOrFail4.java
@@ -0,0 +1,11 @@
+class PassOrFail4 {
+  public static void main(String[] args){
+    int science_mark ;
+    science_mark=75;
+    if (science_mark > 40)
+    System.out.println("Pass");
+    System.out.println("Give a gift");
+    else
+    System.out.println("Fail");
+  }
+}
