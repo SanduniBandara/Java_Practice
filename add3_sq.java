9e64d4020751f3702344bc0a0fc8451e50b947fd
SanduniBandara <85072284+SanduniBandara@users.noreply.github.com>
2021-06-25 23:24:40 +0530

    Sequence of 1 to 60

---
+++ add3_sq.java
@@ -0,0 +1,12 @@
+class add3_sq{
+  public static void main(String [] args){
+    /*Here i is chosen for the numbers in the sequence.
+    Since the sequence start from 1, initial value of i is 1.
+    There is a increment of 3 for each of the element in this
+    sequence.
+    Instead of usual increment of i++/i=i+1 here i=i+3 is used.*/
+    for (int i = 1; i<=60; i=i+3){
+      System.out.println(i);
+    }
+  }
+}
